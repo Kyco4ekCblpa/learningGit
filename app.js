7366f9fd25b75b6afb1ad63bb3a57ff222787d5d
Artem <syropyatov.0000@gmail.com>
2021-02-23 20:20:25 +0200

    first test commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// alert('Hello');
+// confirm('Are you sure?');
+// console.log(prompt('Are you older then 18?', ''));
+
+let answers = [];
+
+
+let n = 5,
+    a = 10,
+    b = 15;
+
+console.log(b % a);
